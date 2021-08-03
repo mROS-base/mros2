17752b250334f79580c34ee507efe98bdda3d579
ryoto.takashima <ryoto.takashima@esm.co.jp>
2021-08-03 10:02:36 +0900

    fix file structure

---
+++ include/mros2_user_config.h
@@ -0,0 +1,14 @@
+#ifndef _MROS2_USER_CONFIG_H_
+#define _MROS2_USER_CONFIG_H_
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+extern int mros2_get_submsg_count(void);
+
+#ifdef __cplusplus
+ }
+#endif
+
+#endif /* _MROS2_USER_CONFIG_H_ */
