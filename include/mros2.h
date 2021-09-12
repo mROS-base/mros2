75be02bcac37433ea680136e944c4ba8f8df34cf
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2021-09-12 20:01:16 +0900

    reorder statements

--- include/mros2.h
+++ include/mros2.h
@@ -6,7 +6,7 @@
 #include "lwip.h"
 #include "mros2/logging.h"
 
-// To avoid link error
+/* Statement to avoid link error */
 #ifdef __cplusplus
 extern void* __dso_handle;
 #endif
@@ -14,27 +14,22 @@ extern void* __dso_handle;
 namespace mros2
 {
 
-class Subscriber
-{
-public:
-  std::string topic_name;
-  static void callback_handler(
-    void* callee,
-    const rtps::ReaderCacheChange& cacheChange
-  );
-  void (*cb_fp)(intptr_t);
-private:
-};
+void init(int argc, char *argv);
 
-class Publisher
-{
-public:
-  std::string topic_name;
-  template <class T>
-  void publish(T& msg);
-};
+#ifdef __cplusplus
+extern "C" {
+#endif
+void mros2_init(void *arg);
+#ifdef __cplusplus
+}
+#endif
 
-class Node//TODO: move to node.h/cpp?
+class Node;
+class Publisher;
+class Subscriber;
+
+/* TODO: move to node.h/cpp? */
+class Node
 {
 public:
   static Node create_node();
@@ -56,23 +51,29 @@ private:
 
 };
 
+class Publisher
+{
+public:
+  std::string topic_name;
+  template <class T>
+  void publish(T& msg);
+};
 
-void init(int argc, char *argv);
+class Subscriber
+{
+public:
+  std::string topic_name;
+  static void callback_handler(
+    void* callee,
+    const rtps::ReaderCacheChange& cacheChange
+  );
+  void (*cb_fp)(intptr_t);
+private:
+};
+
 void spin();
 
-void setTrue(void *args);
-void setTrue(void *args);
-void message_callback(void* callee, const rtps::ReaderCacheChange& cacheChange);
-void message_callback(void* callee, const rtps::ReaderCacheChange& cacheChange);
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-void mros2_init(void *arg);
-#ifdef __cplusplus
-}
-#endif
-}//namespace mros2
+}  /* namespace mros2 */
 
 namespace message_traits
 {
@@ -80,6 +81,6 @@ template <class T>
 struct TypeName {
 static const char* value();
 };
-}// namespace message_traits
-#endif //MROS2_MROS2_H
+}  /* namespace message_traits */
 
+#endif /* MROS2_MROS2_H */
