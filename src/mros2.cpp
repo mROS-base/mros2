496cfdffbc7124f64654dabcf704c8dda1d0eaba
檜原陽一郎 <hibarayoichiro871@gmail.com>
2021-12-09 00:06:53 +0900

    for trying giving class info to cb_handler

--- src/mros2.cpp
+++ src/mros2.cpp
@@ -234,8 +234,8 @@ Subscriber Node::create_subscription(std::string topic_name, int qos, void (*fp)
 template <class T>
 void Subscriber::callback_handler(void *callee, const rtps::ReaderCacheChange &cacheChange)
 {
-  T *msg;
-  msg.copyFromBuf(&cacheChange.data[4]);
+  T msg{};
+  msg->copyFromBuf(&cacheChange.data[4]);
 
   SubscribeDataType *sub = (SubscribeDataType *)callee;
   void (*fp)(intptr_t) = sub->cb_fp;
