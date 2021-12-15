cc39517ded8d6d80ad2ae5a9eef0faae7454d5c4
檜原陽一郎 <hibarayoichiro871@gmail.com>
2021-12-15 18:18:46 +0900

    try move declaration of template functions to header file

--- src/mros2.cpp
+++ src/mros2.cpp
@@ -310,7 +310,7 @@ template void mros2::Subscriber::callback_handler<std_msgs::msg::Byte>(void *cal
 template mros2::Publisher mros2::Node::create_publisher<std_msgs::msg::Char>(std::string topic_name, int qos);
 template mros2::Subscriber mros2::Node::create_subscription(std::string topic_name, int qos, void (*fp)(std_msgs::msg::Char*));
 template void mros2::Publisher::publish(std_msgs::msg::Char &msg);
-template void mros2::Subscriber::callback_handler<std_msgs::msg::>(void *callee, const rtps::ReaderCacheChange &cacheChange);
+template void mros2::Subscriber::callback_handler<std_msgs::msg::Char>(void *callee, const rtps::ReaderCacheChange &cacheChange);
 
 template mros2::Publisher mros2::Node::create_publisher<std_msgs::msg::Float32>(std::string topic_name, int qos);
 template mros2::Subscriber mros2::Node::create_subscription(std::string topic_name, int qos, void (*fp)(std_msgs::msg::Float32*));
